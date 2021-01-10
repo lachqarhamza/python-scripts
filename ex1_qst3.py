237d37d8e3f554de0dd1b00876c90d2215518c99
root <root@localhost.localdomain>
2021-01-10 13:34:42 +0100

    ex1 qst3

---
+++ ex1_qst3.py
@@ -0,0 +1,32 @@
+a = input("enter a\n")
+b = input("enter b\n")
+# cast a and b to int
+a = int(a)
+b = int(b)
+# take the small and the big variable
+if (a > b):
+    # m is the small variable
+    m = b
+    # M is the big variable
+    M = a
+else:
+    m = a
+    M = b
+# seperate the result output
+print("the inverse of integers between", a, "and", b, "are:")
+# initialize a sum variable with 0
+s = 0.0
+# initialize a count variable with 0
+count = 0
+# start the loop
+for i in range(m + 1, M, 1):
+    print(float(1 / i))
+    s += float(1 / i)
+    count += 1
+if (count != 0):
+    mean = float(s) / count
+else:
+    print("-- no results  --")
+    mean = 0.0
+print("their sum is:", s)
+print("their mean is:", mean)
