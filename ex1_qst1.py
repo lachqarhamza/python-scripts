b0e1ae2d4297dbbd95568436f198ebc6c875667c
root <root@localhost.localdomain>
2021-01-10 10:34:20 +0100

    ex1 qst 1

---
+++ ex1_qst1.py
@@ -0,0 +1,23 @@
+a = input("enter a\n")
+b = input("enter b\n")
+# cast a and b to int
+a = int(a)
+b = int(b)
+# take the small and the big variable
+if (a > b):
+    # m is the small variable
+    m = b
+    # M is the big variable
+    M = a
+else:
+    m = a
+    M = b
+# seperate the result output
+print("integers between", a, "and", b, "are:")
+# initialize a sum variable with 0
+s = 0
+# start the loop
+for i in range(m + 1, M, 1):
+    print(i)
+    s += i
+print("their sum is:", s)
