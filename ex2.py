4bb149e64b9e0e1929a98ca20032394b3e6026c4
root <root@localhost.localdomain>
2021-01-10 14:17:28 +0100

    ex9

---
+++ ex2.py
@@ -0,0 +1,9 @@
+a = input ("inter an integer:\n")
+a = int(a)
+if (a < 0):
+    print ("-- enter a number greater than 0 --")
+elif (a == 0):
+    print ("no")
+temp = a
+while temp >= 1:
+    
---
+++ ex9.py
@@ -0,0 +1,10 @@
+i = 4
+while i > 0:
+    j = i
+    while j > 0:
+        if (j == 1):
+            print("*")
+        else:
+            print("* ", end = '')
+        j -= 1
+    i -= 1
