a52a89011a0ae82e6e81b2efbe4c9ea354845786
root <root@localhost.localdomain>
2021-01-10 14:28:36 +0100

    ex8

---
+++ ex8.py
@@ -0,0 +1,8 @@
+i = 1
+while i <= 9:
+    j = 1
+    while j <= 9:
+        print (i * j, " ", end = '')
+        j += 1
+    print ("\n")
+    i += 1
--- ex9.py
+++ ex9.py
@@ -1,10 +1,14 @@
+# initialize i variable with 4
 i = 4
+# start the loop
 while i > 0:
     j = i
     while j > 0:
         if (j == 1):
             print("*")
         else:
+            # end = '' used to force the program
+            # to cary on printing * in the same line
             print("* ", end = '')
         j -= 1
     i -= 1
