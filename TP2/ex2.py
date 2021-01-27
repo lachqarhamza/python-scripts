1dfaf8e90323a820f130c00b9eaec557dc5f619a
root <root@localhost.localdomain>
2021-01-28 00:53:10 +0100

    adding ex2

--- TP2/ex2.py
+++ TP2/ex2.py
@@ -1,24 +1,18 @@
-def read_array_10():
-    # init the list
-    l = []
+def min_max():
+    # get the length of the list
+    n = int(input("enter the number of elements :\n"))
+    # get the list
+    l = list(map(int, input("enter the elements :\n").strip().split()))[:n]
     i = 0
-    print("enter the list's elements\n")
-    while i < 10:
-        # read the element from standard input
-        e = float(input())
-        # add the element to the list
-        l.append(e)
+    m = M = 0
+    # search for min and max
+    while i < n:
+        if l[i] < m:
+            m = l[i]
+        elif l[i] > M:
+            M = l[i]
         i += 1
-    # reverse the list
-    i = 0
-    j = 9
-    while i < j:
-        temp = l[i]
-        l[i] = l[j]
-        l[j] = temp
-        i += 1
-        j -= 1
-    # print the list
-    print(l)
+    # print results
+    print("min = ", m, "max = ", M)
 # to test the function uncomment the line bellow
-# read_array_10(i)
+# min_max()
